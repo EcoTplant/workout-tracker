81994d7e1ca4d580e97a87dc7f1ffe3ca61dfc56
EcoTplant <ecotplant@gmail.com>
2025-10-03 13:18:20 -0500

    creación de recursos con POST

--- src/routes/excercises-routes.js
+++ src/routes/excercises-routes.js
@@ -4,36 +4,20 @@ const exercisesController = require('../../controllers/exercises.controller');
 
 router.use((req, res, next) => {
   req.exercises = [
-    {
-      id: 1,
-      nombre: "Press de Banca",
-      descripcion: "Ejercicio compuesto para desarrollar la fuerza del pecho, hombros y tríceps.",
-      categoria: "fuerza",
-      grupo_muscular: "pecho"
-    },
-    {
-      id: 2,
-      nombre: "Sentadillas",
-      descripcion: "Ejercicio fundamental para piernas y glúteos.",
-      categoria: "fuerza",
-      grupo_muscular: "piernas"
-    },
-    {
-      id: 3,
-      nombre: "Dominadas",
-      descripcion: "Ejercicio para espalda y brazos usando el peso corporal.",
-      categoria: "fuerza",
-      grupo_muscular: "espalda"
-    }
+    { id: 1, nombre: "Press de Banca", descripcion: "Ejercicio compuesto para desarrollar la fuerza del pecho, hombros y tríceps.", categoria: "fuerza", grupo_muscular: "pecho" },
+    { id: 2, nombre: "Sentadillas", descripcion: "Ejercicio fundamental para piernas y glúteos.", categoria: "fuerza", grupo_muscular: "piernas" },
+    { id: 3, nombre: "Dominadas", descripcion: "Ejercicio para espalda y brazos usando el peso corporal.", categoria: "fuerza", grupo_muscular: "espalda" }
   ];
   next();
 });
 
-// Rutas GET implementadas
+// Rutas GET
 router.get('/', exercisesController.getAllExercises);
 router.get('/:id', exercisesController.getExerciseById);
 
-router.post('/');
+// POST implementado
+router.post('/', exercisesController.createExercise);
+
 router.put('/:id');
 router.patch('/:id');
 router.delete('/:id');
