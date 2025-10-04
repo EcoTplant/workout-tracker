2ed62e6670c17abbaf59684b07d598503eb6e849
EcoTplant <ecotplant@gmail.com>
2025-10-04 09:29:46 -0500

    #

--- src/routes/v1/users-routes.js
+++ src/routes/v1/users-routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const usersController = require('../../controllers/users.controller');
+const usersController = require('../../controllers/users-controller');
 
 router.use((req, res, next) => {
   req.users = [
